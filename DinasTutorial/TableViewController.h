61a74d06dcbae9822f42f1ff2fcca9148edc279c
Dina Brunstein <dina_brunstein@moma.org>
2014-05-13 13:20:53 -0400

    WIP: TableView example

---
+++ DinasTutorial/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  DinasTutorial
+//
+//  Created by pivotal on 5/13/14.
+//  Copyright (c) 2014 pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UIViewController
+
+@end
